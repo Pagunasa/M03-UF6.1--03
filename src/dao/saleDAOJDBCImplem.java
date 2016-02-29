6b36fec7a9ca54074f08b76cfc07c8150e275079
kebuac <kevinbustillos21@hotmail.com>
2016-02-29 20:01:21 +0100

    Haciendo saleDAOImplem

--- src/dao/saleDAOJDBCImplem.java
+++ src/dao/saleDAOJDBCImplem.java
@@ -2,14 +2,42 @@ package dao;
 
 import exceptions.DAOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import models.Client;
 import models.Sale;
+import singleton.DatabaseConnection;
 
-public class saleDAOJDBCImplem implements interfaceDAO<Sale, Integer>{
 
-    @Override
-    public ArrayList<Sale> list(Connection connection) throws DAOException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+public class saleDAOJDBCImplem{
+
+    public ArrayList<Sale> listSalesByClient(Client client, Connection connection) throws DAOException{
+        ArrayList<Sale> salesArray = new ArrayList<>();
+        client = new Client();
+        String selectQuery = "SELECT * FROM sales WHERE client_cif = " +client.getCif();
+        
+        try {
+            connection = DatabaseConnection.getInstance();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(selectQuery);
+            
+            while(resultSet.next()){
+                client.setCif(resultSet.getString(0));
+                client.setName(resultSet.getString(1));
+                client.setDirection(resultSet.getString(2));
+                client.setTown(resultSet.getString(3));
+                client.setTelephone(resultSet.getString(4));
+            }
+            
+        }catch(SQLException ex){
+            System.out.println(ex.getMessage());
+        }
     }
-
+    
+    public void insertSales(String query) throws DAOException{
+        
+    }
 }
