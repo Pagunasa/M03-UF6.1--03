19de9238549440cd7a0626ddb8695eaafd0887f6
kebuac <kevinbustillos21@hotmail.com>
2016-03-06 03:00:03 +0100

    Renombrando clases y haciendo DAOFactory

--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -1,5 +1,20 @@
 package dao;
 
+import models.Client;
+import models.Product;
+import models.Sale;
+
 public class DAOFactory {
     
+    public static Client createClient(){
+        return new Client();
+    }
+    
+    public static Product createProduct(){
+        return new Product();
+    }
+    
+    public static Sale createSale(){
+        return new Sale();
+    }
 }
